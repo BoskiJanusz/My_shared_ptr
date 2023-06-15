4016733bd0b551327c021bb8d73bb9104ed418e0
BoskiJanusz <patmich@onet.eu>
2023-06-15 18:26:14 +0200

    move constructor implementation

--- src/shared_ptr.hpp
+++ src/shared_ptr.hpp
@@ -12,7 +12,13 @@ namespace my
         shared_ptr() : ptr_(nullptr), counter_(new size_t{0}){}
         shared_ptr(T* ptr) : ptr_(ptr), counter_(new size_t{1}){}
         shared_ptr(const shared_ptr &){}
-        shared_ptr(shared_ptr &&){}
+        shared_ptr(shared_ptr && obj)
+        {
+            ptr_ = obj.ptr_;
+            counter_ = obj.counter_;
+            obj.ptr_ = nullptr;
+            obj.counter_ = nullptr;
+        }
         shared_ptr& operator=(const shared_ptr &){}
         shared_ptr& operator=(shared_ptr &&){}
         operator bool() const;
