9339eb14c742e6cdec005ed6fb602cb425245a53
BoskiJanusz <patmich@onet.eu>
2023-06-15 16:48:46 +0200

    added constructor and arrow operator implementation

--- src/shared_ptr.hpp
+++ src/shared_ptr.hpp
@@ -10,13 +10,16 @@ namespace my
         size_t* counter_ = nullptr;
     public:
         shared_ptr(){}
-        shared_ptr(T* ptr){}
+        shared_ptr(T* ptr) : ptr_(ptr), counter_(new size_t{1}){}
         shared_ptr(const shared_ptr &){}
         shared_ptr(shared_ptr &&){}
         shared_ptr& operator=(const shared_ptr &){}
         shared_ptr& operator=(shared_ptr &&){}
         operator bool() const;
-        T* operator->() const;
+        T* operator->() const
+        {
+            return ptr_;
+        }
         T& operator*() const;
         T* get() const;
         void reset();
