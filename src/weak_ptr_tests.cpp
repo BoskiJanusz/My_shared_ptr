5f78ed7040292762251d2fa656e43f6345d6b353
BoskiJanusz <patmich@onet.eu>
2023-07-05 20:50:25 +0200

    test for reset function

--- src/weak_ptr_tests.cpp
+++ src/weak_ptr_tests.cpp
@@ -27,6 +27,18 @@ TEST(weak_ptrTest, FunctionLockWeakPtrOtherShared) {
     EXPECT_EQ(expected, value);
 }
 
+TEST(weak_ptrTest, FunctionResetDifferentUseCountResults) {
+    my::shared_ptr<int> shared_ptr1(new int{10});
+    my::shared_ptr<int> shared_ptr2(shared_ptr1);
+    my::shared_ptr<int> shared_ptr3(shared_ptr1);
+    my::weak_ptr<int> weak_ptr(shared_ptr1);
+    weak_ptr.reset();
+    auto value = weak_ptr.use_count();
+    auto expected = 0;
+
+    EXPECT_EQ(expected, value);
+}
+
 TEST(weak_ptrTest, ConstructorWeakPtrEqualsSharedPtr) {
     my::shared_ptr<int> shared_ptr(new int{10});
     my::weak_ptr<int> weak_ptr(shared_ptr);
@@ -63,7 +75,7 @@ TEST(weak_ptrTest, CopyAssignmentCounterEqualsTwoTest) {
     my::weak_ptr<int> weak_ptr2(shared_ptr2);
     weak_ptr1 = weak_ptr2;
     auto ptrCounter = weak_ptr1.use_count();
-    auto expected = 1;
+    auto expected = 2;
 
     EXPECT_EQ(expected, ptrCounter);
 }
